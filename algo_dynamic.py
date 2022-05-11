536100916e99f6e7f65c9e239428cfc21f507309
chris <christopher.poncelet51@gmail.com>
2022-05-11 14:54:40 +0200

    algo dynamic

---
+++ algo_dynamic.py
@@ -0,0 +1,43 @@
+from utils.data_usefull import MAX_EXPENSE
+from utils.get_data_from_csv import get_data_from_csv
+
+
+def algo_dynamic(file) -> dict:
+    """
+    On créé une matrice, une sorte de tableau comportant autant de ligne que d'élément et de colonne que de capacité.
+    Chaque élément sera comparé afin de trouver le résultat le plus optimal
+
+    :param file:
+    """
+
+    stocks = get_data_from_csv(file)
+
+    matrix = [[0 for x in range(MAX_EXPENSE + 1)] for x in range(len(stocks) + 1)]
+
+    for i in range(1, len(stocks) + 1):
+        # Pour chaque élément on parcourt la capacité: ligne de la matrice
+        for w in range(1, MAX_EXPENSE + 1):
+
+            # On vérifie si le montant de l'élément de la ligne précédente est inférieur ou égal que la capacité
+            # dans la matrice, on vérifie si le bénéfice est supérieur à celui de la ligne précédente
+            if stocks[i-1].amount <= w:
+                matrix[i][w] = max(stocks[i-1].profit + matrix[i-1][w-stocks[i-1].amount], matrix[i-1][w])
+            else:
+                # On garde l'élément optimiser de la ligne d'avant
+                matrix[i][w] = matrix[i-1][w]
+
+    # Retrouver les éléments en fonction de la somme
+    max_expense = MAX_EXPENSE
+    n = len(stocks)
+    elements_selection = []
+
+    while max_expense >= 0 and n >= 0:
+        e = stocks[n-1]
+
+        if matrix[n][max_expense] == matrix[n-1][max_expense-e.amount] + e.profit:
+            elements_selection.append(e)
+            max_expense -= e.amount
+
+        n -= 1
+
+    return {"Total bénéfice": matrix[-1][-1], "Combinaisons d'actions": elements_selection}
--- main.py
+++ main.py
@@ -1,11 +1,40 @@
+from algo_dynamic import algo_dynamic
 from bruteforce import brute_force
 
-
-csv_file = open("./csv/action-test5.csv")
+"""
+Le même fichier est appelé deux fois dans deux variables différentes car sinon une erreur de type 
+"IndexError: list index out of range" se déclenche
+"""
+csv_file1 = open("./csv/action-test5.csv")
+csv_file2 = open("./csv/action-test5.csv")
 
 
 def run():
-    print(brute_force(csv_file))
+
+    choices = {
+        "1": algo_dynamic(csv_file1),
+        "2": brute_force(csv_file1),
+    }
+
+    while True:
+        print()
+        print("Sélectionnez l'algorithme de votre choix :\n\n"
+              "1 - Algorithme dynamique\n"
+              "2 - Algorithme Force Brute\n"
+              "Q - Quitter")
+        asked = input(">>> ")
+        print("\n")
+
+        result = choices.get(asked)
+
+        if asked == "q".lower():
+            break
+
+        elif asked not in choices:
+            print("Sélectionnez une valeur correcte")
+
+        else:
+            print(result)
 
 
 if __name__ == "__main__":
